bc9414226358e784261bdc2088f3177c5c5d3829
yeeun <103231425+corgiLoaf@users.noreply.github.com>
2024-10-30 19:06:04 +0900

    🔥 branch 범위에 해당하지 않는 작업 삭제

--- src/event/type/event-status.type.ts
+++
@@ -1,7 +0,0 @@
-export const EventStatus = {
-    PENDING: 'PENDING' // 시작 전
-    ONGOING: 'ONGOING' // 진행 중
-    COMPETED: 'COMPLETED' // 종료
-  };
-  
-export type EventStatus = (typeof EventStatus)[keyof typeof EventStatus];
